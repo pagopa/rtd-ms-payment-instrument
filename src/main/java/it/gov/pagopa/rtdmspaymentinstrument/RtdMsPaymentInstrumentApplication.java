84c6644a970e465b7b38a699f495cdbcc927bc0d
Luca Consalvi <luca.consalvi@pagopa.it>
2023-02-07 16:51:35 +0100

    initialized java spring boot 3 project with gradle, added codeowners and pr template

---
+++ src/main/java/it/gov/pagopa/rtdmspaymentinstrument/RtdMsPaymentInstrumentApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.rtdmspaymentinstrument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtdMsPaymentInstrumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtdMsPaymentInstrumentApplication.class, args);
+	}
+
+}
