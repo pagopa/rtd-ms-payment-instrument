42b1e8f0af97e3811b6bdf174d442c977995e640
Luca Consalvi <luca.consalvi@pagopa.it>
2023-02-16 14:42:02 +0100

    [RTD-1339] added event handler and service to process migration event

---
+++ src/main/java/it/gov/pagopa/rtdmspaymentinstrument/event/EventHandler.java
@@ -0,0 +1,23 @@
+package it.gov.pagopa.rtdmspaymentinstrument.event;
+
+
+import java.util.function.Consumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import it.gov.pagopa.rtdmspaymentinstrument.service.PaymentManagerMigrationProcessor;
+
+/**
+ * /** Component defining the processing steps in response to events in the projector queue.
+ */
+@Slf4j
+@Configuration
+public class EventHandler {
+
+    @Bean
+    public Consumer<Message<String>> migrationPiConsumer(
+            PaymentManagerMigrationProcessor paymentManagerMigrationProcessor) {
+        return message -> paymentManagerMigrationProcessor.processing(message.getPayload());
+    }
+}
---
+++ src/main/java/it/gov/pagopa/rtdmspaymentinstrument/service/PaymentManagerMigrationProcessor.java
@@ -0,0 +1,9 @@
+package it.gov.pagopa.rtdmspaymentinstrument.service;
+
+public class PaymentManagerMigrationProcessor {
+
+    public void processing(String message) {
+        System.out.println(message);
+    }
+
+}
