b97f8bcd4aa1cf2276d62928e2a025f25ba32097
Luca Consalvi <luca.consalvi@pagopa.it>
2023-02-16 18:13:02 +0100

    [RTD-1339] add servuce to PaymentInstumentApplication

--- src/main/java/it/gov/pagopa/rtdmspaymentinstrument/service/PaymentManagerMigrationProcessor.java
+++ src/main/java/it/gov/pagopa/rtdmspaymentinstrument/service/PaymentManagerMigrationProcessor.java
@@ -1,8 +1,10 @@
 package it.gov.pagopa.rtdmspaymentinstrument.service;
 
+import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Service
 public class PaymentManagerMigrationProcessor {
 
     public void processing(String message) {
